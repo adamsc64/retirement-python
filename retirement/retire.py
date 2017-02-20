0cbb063f5e31482d8836a6b15e81431906a27276
Christopher Adams <christopher.r.adams@gmail.com>
2017-02-19 20:36:07 -0500

    Require salary and percentage contribution

--- retirement/retire.py
+++ retirement/retire.py
@@ -5,14 +5,16 @@ import datetime
 
 from vanguard import Vanguard
 
+ANNUAL_RAISE = 3.0
+
 
 class Fund(object):
     def __init__(self, growth, start_with=0):
         self.growth = growth
         self.total = start_with
 
-    def do_month(self, per_month, yields):
-        adding = float(per_month)
+    def do_month(self, contribution, yields):
+        adding = float(contribution)
         self.total += adding
         gain = self.total * yields
         gain = (int(gain * 100.0) / 100.0)
@@ -20,15 +22,17 @@ class Fund(object):
         return gain
 
 
-def retire(per_month, num_years, growth, start_with=0.0):
+def retire(salary, contribution, num_years, growth, start_with=0.0):
     vanguard = Vanguard(target_year=num_years)
     fund = Fund(growth=vanguard, start_with=start_with)
     for year in range(num_years):
+        salary *= (1 + (ANNUAL_RAISE / 100.0))
         for month in range(0, 12):
             year_allocation = vanguard.get_allocation(num_years - year)
             yields_per_year = vanguard.get_yield(year_allocation)
             yields_per_month = yields_per_year / 12.0
-            gain = fund.do_month(per_month=per_month, yields=yields_per_month)
+            monthly = (salary / 12.0) * (contribution / 100.0)
+            gain = fund.do_month(contribution=monthly, yields=yields_per_month)
             inflation_total = fund.total * math.pow(1.0 - (3.22 / 100.0), year)
             print ("Year {}, "
                    "Month {}, "
@@ -39,7 +43,7 @@ def retire(per_month, num_years, growth, start_with=0.0):
                    ).format(
                     year + 1,
                     month + 1,
-                    per_month,
+                    monthly,
                     gain,
                     fund.total,
                     inflation_total,
@@ -49,8 +53,10 @@ def retire(per_month, num_years, growth, start_with=0.0):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Calculate projected investment total each month.')
-    parser.add_argument('--per-month', type=int, required=True,
-        help='Amount to save per month, in dollars.')
+    parser.add_argument('--salary', type=int, required=True,
+        help='Annual salary.')
+    parser.add_argument('--contribution', type=int, required=True,
+        help='Contribution of salary per month, in percent.')
     parser.add_argument('--num-years', type=int, required=True,
         help='Number of years to save.')
     parser.add_argument('--growth', type=float, default=5.5,
@@ -58,4 +64,4 @@ if __name__ == "__main__":
     parser.add_argument('--start-with', type=int, default=0,
         help='Starting amount.')
     args = parser.parse_args()
-    retire(args.per_month, args.num_years, args.growth, args.start_with)
+    retire(args.salary, args.contribution, args.num_years, args.growth, args.start_with)
